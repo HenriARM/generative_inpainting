ac6c384c5b296c57ba75bedb1fd0ba151e534024
Henry <henriarm@yandex.ru>
2021-06-16 16:03:49 +0300

    Added protobuf saver and inference migrated to tf2

--- inference.py
+++ inference.py
@@ -4,10 +4,12 @@ import os
 import utils
 import glob
 import argparse
-import neuralgym as ng
+import neuralgymtf2 as ng
 
 import tensorflow as tf
 from inpaint_model import InpaintCAModel
+
+tf.compat.v1.disable_eager_execution()
 
 CHECKPOINT_DIR = './placesv2-512'
 INPUT_SIZE = 256  # input image size for Generator 512
@@ -18,35 +20,58 @@ INPAINT_SUFFIX = '_inpainted.png'
 MIN_BBOX_AREA = 50 * 50
 OVERLAP_DISTANCE = 200
 
+# def inferencetf2(image, mask):
+#     # prepare input tensor
+#     mask = np.dstack([mask] * 3)
+#     mask = np.expand_dims(mask, 0)
+#     image = np.expand_dims(image, 0)
+#     input_image = np.concatenate([image, mask], axis=2)
+    
+#     with tf.compat.v1.Session(graph=tf.compat.v1.Graph()) as sess:
+#         tf.compat.v1.saved_model.loader.load(sess, ['serve'], './modeltf2')
+#         graph = tf.compat.v1.get_default_graph()
+#         input_tensor = graph.get_tensor_by_name('Placeholder:0')
+#         output_tensor = graph.get_tensor_by_name('saturate_cast:0')
+#         # inference
+#         output_image = sess.run(output_tensor,feed_dict={input_tensor: input_image})
+#         output_image = output_image[0][:, :, ::-1]
+#         return output_image
 
 def inference(image, mask):
     FLAGS = ng.Config('inpaint.yml')
-    sess_config = tf.ConfigProto()
+    sess_config = tf.compat.v1.ConfigProto()
     sess_config.gpu_options.allow_growth = True
-    sess = tf.Session(config=sess_config)
+    sess = tf.compat.v1.Session(config=sess_config)
 
     model = InpaintCAModel()
-    input_image_ph = tf.placeholder(tf.float32, shape=(1, INPUT_SIZE, INPUT_SIZE * 2, 3))
-    output = model.build_server_graph(FLAGS, input_image_ph, reuse=tf.AUTO_REUSE)
+    input_image_ph = tf.compat.v1.placeholder(tf.float32, shape=(1, INPUT_SIZE, INPUT_SIZE * 2, 3))
+    output = model.build_server_graph(FLAGS, input_image_ph, reuse=tf.compat.v1.AUTO_REUSE)
     output = (output + 1.) * 127.5
     output = tf.reverse(output, [-1])
     output = tf.saturate_cast(output, tf.uint8)
-    vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+    vars_list = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_VARIABLES)
     assign_ops = []
     for var in vars_list:
         vname = var.name
         from_name = vname
-        var_value = tf.contrib.framework.load_variable(CHECKPOINT_DIR, from_name)
-        assign_ops.append(tf.assign(var, var_value))
+        var_value = tf.train.load_variable(CHECKPOINT_DIR, from_name)
+        assign_ops.append(tf.compat.v1.assign(var, var_value))
     sess.run(assign_ops)
 
+    # # save model as protobuf
+    # graph = sess.graph
+    # input_tensor = graph.get_tensor_by_name('Placeholder:0')
+    # output_tensor = graph.get_tensor_by_name('saturate_cast:0')
+    # tf.compat.v1.saved_model.simple_save(
+    #     sess,"./modeltf2",
+    #     inputs={'Placeholder:0':input_tensor}, 
+    #     outputs={'saturate_cast:0': output_tensor}
+    # )
     # TODO: change
     mask = np.dstack([mask] * 3)
-
     image = np.expand_dims(image, 0)
     mask = np.expand_dims(mask, 0)
     input_image = np.concatenate([image, mask], axis=2)
-
     # load pretrained model
     output_image = sess.run(output, feed_dict={input_image_ph: input_image})
     output_image = output_image[0][:, :, ::-1]
